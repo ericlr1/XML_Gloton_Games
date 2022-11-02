72379cdd650889b220ff8f6b1d4127e8418d0ad3
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2022-11-02 14:19:42 +0100

    pARED

--- Gloton Galactico/Game/Source/Scene.cpp
+++ Gloton Galactico/Game/Source/Scene.cpp
@@ -7,6 +7,7 @@
 #include "Scene.h"
 #include "EntityManager.h"
 #include "Map.h"
+#include "Player.h"
 
 #include "Defs.h"
 #include "Log.h"
@@ -79,7 +80,7 @@ bool Scene::Update(float dt)
 	if (app->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
 		app->LoadGameRequest();
 
-	if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)
+	/*if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)
 		app->render->camera.y += 3;
 
 	if (app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT)
@@ -89,9 +90,10 @@ bool Scene::Update(float dt)
 		app->render->camera.x += 3;
 
 	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
-		app->render->camera.x -= 3;
+		app->render->camera.x -= 3;*/
 
 	//app->render->DrawTexture(img, 380, 100); // Placeholder not needed any more
+	app->render->camera.x = -player->position.x + 100;
 
 	// Draw map
 	app->map->Draw();
