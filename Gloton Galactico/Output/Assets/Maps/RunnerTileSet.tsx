30ec14b04796803e0d903417cea92ddb02b9bbc3
ericlr1 <ericluque2003@gmail.com>
2022-11-02 16:56:49 +0100

    Fondo pocho pero pasable

---
+++ Gloton Galactico/Output/Assets/Maps/RunnerTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="RunnerTileSet" tilewidth="32" tileheight="32" tilecount="78" columns="13">
+ <image source="../../../../Tileset provisional/Space Runner Assets - 27-11-20/Space Runner Assets - 27-11-20/Tiles/RunnerTileSet.png" width="432" height="192"/>
+</tileset>
