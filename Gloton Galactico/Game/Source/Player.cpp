42f9ff2be14b6967906a83d0ab0194689de1a7b6
LordSalsa <0208marcelsunyer@gmail.com>
2022-11-11 12:00:29 +0100

    a

--- Gloton Galactico/Game/Source/Player.cpp
+++ Gloton Galactico/Game/Source/Player.cpp
@@ -49,7 +49,7 @@ bool Player::Start() {
 	vidaTexture = app->tex->Load("Assets/Textures/vida.png");
 
 	// L07 DONE 5: Add physics to the player - initialize physics body
-	pbody = app->physics->CreateCircle(position.x+16, position.y+16, 16, bodyType::DYNAMIC);
+	pbody = app->physics->CreateCircle(position.x, position.y+200, 16, bodyType::DYNAMIC);
 
 	// L07 DONE 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
 	pbody->listener = this; 
